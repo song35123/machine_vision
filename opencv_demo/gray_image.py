2fc54d4e1c99ed6041ad46c9f964080b000923db
song35123 <2869340644@qq.com>
2026-04-09 15:10:10 +0800

    Create gray_image.py

---
+++ opencv_demo/gray_image.py
@@ -0,0 +1,10 @@
+import cv
+image = cv.imread("test.jpg")
+
+if image is None:
+  print("图片读取失败，请检查路径是否正确")
+else:
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  cv2.imshow("Gray Image", gray)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
