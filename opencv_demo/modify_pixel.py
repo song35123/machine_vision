8f6c1a04086fc9668017262bc8b2e275c3facb49
song35123 <2869340644@qq.com>
2026-04-19 19:33:07 +0800

    Update modify_pixel.py

--- opencv_demo/modify_pixel.py
+++ opencv_demo/modify_pixel.py
@@ -5,7 +5,15 @@ if image is None:
   print("图片读取失败，请检查路径是否正确")
 
 else:
-  image[0:100,0:100] = [0,0,255]
+  height = image.shape[0]
+  width = image.shape[1]
+  half_h = height/2
+  half_w = width/2
+  image[(half_h - 50):(half_h + 50),(half_w - 50):(half_w + 50)] = [0,255,0]
   cv2.imshow("Modified Image", image)
   cv2.waitKey(0)
   cv2.destroyAllWindows()
+  # image[0:100,0:100] = [0,0,255]
+  # cv2.imshow("Modified Image", image)
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()
