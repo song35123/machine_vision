d56c9636443c86bab3280d2e52493c87a3e2f8d5
song35123 <2869340644@qq.com>
2026-04-28 20:59:57 +0800

    added rectangle_center.py and modified detect_rectangle.py

--- opencv_demo/rectangle_center.py
+++ opencv_demo/rectangle_center.py
@@ -6,12 +6,12 @@ if image is None:
     print("无法加载图像，请检查路径是否正确")
 else:
     result = image.copy()
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
     ret,binary = cv2.threshold(blur,127,255,cv2.THRESH_BINARY)
     contours,hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    
+
     for contour in contours:
         area = cv2.contourArea(contour)
         if area < 500:
@@ -22,18 +22,17 @@ else:
         if len(approx) == 4:
             x,y,w,h = cv2.boundingRect(approx)
             rect_area = w * h
-            ratio  = area / rect_area
+            ratio = area / rect_area
             if ratio > 0.81:
-                cv2.drawContours(result,[approx],0,(0,255,0),2)
-                cv2.rectangle(result,(x,y),(x+w,y+h),(0,255,0),0)
+                center_x = x + w//2
+                center_y = y + h//2
 
-                print("检测到疑似矩形")
-                print("轮廓面积:", area)
-                print("外接矩形面积:", rect_area)
-                print("面积比例:", ratio)
-                print("矩形坐标 x, y, w, h:", x, y, w, h)
+                cv2.rectangle(result,(x,y),(x+w,y+h),(0,255,0),2)
+                cv2.circle(result,(center_x, center_y), 5, (0,0,255), -1)
+
+                cv2.putText(result, f"Center: ({center_x}, {center_y})", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,0), 1)
+                print("矩形中心坐标:", (center_x, center_y))
     cv2.imshow("Binary Image", binary)
-    cv2.imshow("Rectangle Detection", result)
+    cv2.imshow("Result", result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
